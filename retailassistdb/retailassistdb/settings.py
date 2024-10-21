a46f6e019cbd30e14b1ae5dfcc52c603931d4145
Don Shammah <donshammah1@gmail.com>
2024-10-21 13:43:07 +0300

    Configured STATIC_ROOT for Heroku

--- retailassistdb/retailassistdb/settings.py
+++ retailassistdb/retailassistdb/settings.py
@@ -21,6 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # settings.py
 import os
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -142,7 +145,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
