d917f61c003259fd755e0e2f589c02711a8b556e
Don Shammah <donshammah1@gmail.com>
2024-10-28 15:20:41 +0300

    edited template files with bootstrap css and js added to static files

---
+++ staticfiles/CACHE/js/output.d38a805abc02.js
@@ -0,0 +1,1 @@
+const tooltipTriggerList=document.querySelectorAll('[data-bs-toggle="tooltip"]');const tooltipList=[...tooltipTriggerList].map(tooltipTriggerEl=>new bootstrap.Tooltip(tooltipTriggerEl));function toggleDarkMode(){document.body.classList.toggle('dark-mode');};
