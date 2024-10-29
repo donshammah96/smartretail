74265a52ea4cb144dd62feab87a0558b7c3789c8
Don Shammah <donshammah1@gmail.com>
2024-10-29 10:35:58 +0300

    Update django-silk and ensure compatibility with Django 4.0+

--- retailassistdb/urls.py
+++ retailassistdb/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import re_path as url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve  # Import the serve function
